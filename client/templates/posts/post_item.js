44809b3249e95d134559b6932822e1663a3ca4d3
Braden Whitten <bwhitten518@gmail.com>
2015-01-10 22:13:02 -0500

    Added edit posts form

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -5,6 +5,10 @@
  */
 
 Template.postItem.helpers({
+  ownPost: function () {
+    return this.userId === Meteor.userId();
+  },
+
   domain: function() {
     var a = document.createElement('a');
     a.href = this.url;
