ef132b67e4b24ec955b4a9262d8bb384930ecba0
Braden Whitten <bwhitten518@gmail.com>
2015-01-21 23:41:44 -0500

    Validate post contents when editing

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -15,6 +15,11 @@ Template.postEdit.events({
       title: $(e.target).find('[name=title]').val()
     };
 
+    var errors = PostHelper.validate(postProperties);
+    if (errors.url || errors.title) {
+      return Session.set('postEditErrors', errors);
+    }
+
     Posts.update(currentPostId, {$set: postProperties}, function (err) {
       if (err) {
         throwError(err.reason);
@@ -32,5 +37,32 @@ Template.postEdit.events({
       Posts.remove(currentPostId);
       Router.go('postsList');
     }
+  }
+});
+
+Template.postEdit.created = function () {
+  Session.set('postEditErrors', {});
+};
+
+Template.postEdit.helpers({
+
+  /**
+   * Returns an error message for a given field.
+   *
+   * @param {string} field
+   * @returns {string}
+   */
+  errorMessage: function (field) {
+    return Session.get('postEditErrors')[field];
+  },
+
+  /**
+   * Returns an error class if an error exists for a given field.
+   *
+   * @param {string} field
+   * @returns {string}
+   */
+  errorClass: function (field) {
+    return Session.get('postEditErrors')[field] ? 'has-error' : '';
   }
 });
